869ecc2b26ae6e8a84adc3ebc439636992c0fb57
Nguyen <87511888+nknguyenhc@users.noreply.github.com>
2024-07-08 16:13:04 +0800

    Add HTML template

--- prompt_injector/urls.py
+++ prompt_injector/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import index
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("products/", include("products.urls")),
+    path("", view=index),
 ]
