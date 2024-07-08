b20c390f3f492b18610481db932d31ac73f973ef
Nguyen <87511888+nknguyenhc@users.noreply.github.com>
2024-07-08 10:02:39 +0800

    Add product fields

--- products/models.py
+++ products/models.py
@@ -3,14 +3,20 @@ from django.db import models
 class Pet(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+    popularity = models.CharField(max_length=20)
+    stock_status = models.CharField(max_length=20)
 
 class Book(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+    popularity = models.CharField(max_length=20)
+    stock_status = models.CharField(max_length=20)
 
 class Movie(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+    popularity = models.CharField(max_length=20)
+    stock_status = models.CharField(max_length=20)
 
 class Flag(models.Model):
     value = models.CharField(max_length=100)
